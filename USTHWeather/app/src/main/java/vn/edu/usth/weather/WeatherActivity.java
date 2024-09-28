861fcf296a0fc620a3874154e3b53687aa41bcc2
minhnguyenquang1709 <minhnguyenquang2004vn@gmail.com>
2024-09-28 08:37:39 +0700

    practical work 11

--- USTHWeather/app/src/main/java/vn/edu/usth/weather/WeatherActivity.java
+++ USTHWeather/app/src/main/java/vn/edu/usth/weather/WeatherActivity.java
@@ -1,6 +1,8 @@
 package vn.edu.usth.weather;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.os.Environment;
 import android.util.Log;
 
 import androidx.activity.EdgeToEdge;
@@ -13,14 +15,47 @@ import androidx.viewpager.widget.ViewPager;
 
 import com.google.android.material.tabs.TabLayout;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+
 public class WeatherActivity extends AppCompatActivity {
     private final String tag = "Weather";
+    private MediaPlayer mp;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         // setTheme(androidx.appcompat.R.style.Theme_AppCompat_DayNight);
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_weather);
+
+        try {
+            InputStream is = getResources().openRawResource(R.raw.erikmusic);
+
+            File sdCard = Environment.getExternalStorageDirectory();
+            File musicFile = new File(sdCard, "my_music.mp3");
+
+            OutputStream outputStream = new FileOutputStream(musicFile);
+            byte[] buffer = new byte[1024];
+            int length;
+            while ((length = is.read(buffer)) > 0) {
+                outputStream.write(buffer, 0, length);
+            }
+            is.close();
+            outputStream.close();
+
+
+
+            mp = new MediaPlayer();
+            mp.setDataSource(musicFile.getAbsolutePath());
+            mp.prepare();
+            mp.start();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+
 
         // setup ViewPager
         PagerAdapter adapter = new HomeFragmentPagerAdapter(getSupportFragmentManager());
